a299c4781eb38ec80608687b7f7802cae26741a8
gonglu666 <38758941+gonglu666@users.noreply.github.com>
2018-05-08 17:03:08 +0800

    Update GitHubTest.java
    the first modify on github!

--- src/test/java/com/ebm/GitHubTest.java
+++ src/test/java/com/ebm/GitHubTest.java
@@ -6,5 +6,7 @@ package com.ebm;
 public class GitHubTest {
     public static void main(String [] args){
         System.out.println("githubtest");
+        System.out.println("the first modify on github!")
+
     }
 }
