30af97b821a7a18caaac0f6b0fc7dbf41b54f1e2
gonglu <g514331301@163.com>
2018-05-08 17:04:55 +0800

    新增GitHubTest，修改HelloController

--- src/test/java/com/ebm/GitHubTest.java
+++ src/test/java/com/ebm/GitHubTest.java
@@ -7,7 +7,7 @@ public class GitHubTest {
     public static void main(String [] args){
         System.out.println("githubtest");
         System.out.println("the first modify on idea!");
-        System.out.println("the first modify on github!")
+        System.out.println("the first modify on github!");
 
     }
 }
